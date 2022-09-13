874e42c159b6904218b664f76d35e13a0d6baf2c
Maninder Singh <maninder.singh12994@gmail.com>
2022-09-14 01:45:46 +0530

    Migration for Cinema

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -37,7 +37,107 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        Schema::create('movies', function($table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('thumbnail');
+            $table->text('about');
+            $table->timestamps();
+        });
+
+        Schema::create('show_types', function($table) {
+            $table->increments('id'); 
+            $table->string('title'); // 2D,3D,IMAX3D
+            $table->timestamps();
+        });
+        Schema::create('movie_show_types', function($table) {
+            $table->increments('id');
+            $table->integer('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->integer('show_type_id')->unsigned();
+            $table->foreign('show_type_id')->references('id')->on('show_types')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('languages', function($table) {
+            $table->increments('id'); 
+            $table->string('title'); // Hindi,Malayalam,Tamil,Punjabi
+            $table->timestamps();
+        });
+
+        Schema::create('movie_languages', function($table) {
+            $table->increments('id'); 
+            $table->integer('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->integer('language_id')->unsigned();
+            $table->foreign('language_id')->references('id')->on('languages')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('genres', function($table) {
+            $table->increments('id'); 
+            $table->string('title'); // Hindi,Malayalam,Tamil,Punjabi
+            $table->timestamps();
+        });
+
+        Schema::create('movie_genres', function($table) {
+            $table->increments('id'); 
+            $table->integer('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->integer('genre_id')->unsigned();
+            $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+
+        Schema::create('casts', function($table) {
+            $table->increments('id'); 
+            $table->string('name'); // Ranboor Kapoor
+            $table->string('image'); // 
+            $table->timestamps();
+        });
+
+        Schema::create('movie_cast', function($table) {
+            $table->increments('id'); 
+            $table->integer('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->integer('cast_id')->unsigned();
+            $table->foreign('cast_id')->references('id')->on('casts')->onDelete('cascade');
+            $table->string('character_name'); // Ranbir as Shiva
+            $table->timestamps();
+        });
+
+        Schema::create('movie_time_slots', function($table) {
+            $table->increments('id'); 
+            $table->integer('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->integer('total_seats');
+            $table->enum('is_sold', ['0', '1'])->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('seat_types', function($table) {
+            $table->increments('id'); 
+            $table->string('title'); // VIP/NORMAL
+            $table->timestamps();
+        });
+
+        Schema::create('pricing', function($table) {
+            $table->increments('id');
+            $table->integer('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->integer('movie_time_slot_id')->unsigned();
+            $table->foreign('movie_time_slot_id')->references('id')->on('movie_time_slots')->onDelete('cascade'); 
+            $table->integer('seat_type_id')->unsigned();
+            $table->foreign('seat_type_id')->references('id')->on('seat_types')->onDelete('cascade');
+            $table->integer('movie_show_type_id')->unsigned();
+            $table->foreign('movie_show_type_id')->references('id')->on('movie_show_types')->onDelete('cascade');
+            $table->integer('price'); //
+            $table->timestamps();
+        });
+        //throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
     }
 
     /**
